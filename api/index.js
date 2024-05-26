adefdce250e7170860636c4f0076320f0e73685d
aztecgarlic <hifgaffoor01@gmail.com>
2024-05-26 20:02:42 +0530

    signup route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.route.js';
 
 mongoose.connect("mongodb+srv://hifgaffoor01:Wer172829@realestate.bo8ql60.mongodb.net/?retryWrites=true&w=majority&appName=realestate").then(()=>{
     console.log('Connected to MongoDB!');
@@ -11,10 +12,14 @@ mongoose.connect("mongodb+srv://hifgaffoor01:Wer172829@realestate.bo8ql60.mongod
 
 const app = express();
 
+app.use(express.json());
+
 app.listen (3000, () => {
     console.log('Server is running on port 3000!!!');
     }
 );
 
 
-app.use('/api/user',userRouter);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+ 
